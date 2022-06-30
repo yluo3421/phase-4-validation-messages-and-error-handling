ef301fd88fb3a83dea454b29bffa55ff29a35e94
root <yluo3421@gmail.com>
2022-06-30 10:34:36 -0400

    commit tree 0eacb20bc9ab24df8aa49038ae7dde669b368efd

--- client/src/components/MovieForm.js
+++ client/src/components/MovieForm.js
@@ -2,6 +2,7 @@ import { useState } from "react";
 import styled from "styled-components";
 
 function MovieForm() {
+  const [errors, setErrors] = useState([]);
   const [formData, setFormData] = useState({
     title: "",
     year: new Date().getFullYear(),
@@ -23,8 +24,13 @@ function MovieForm() {
       },
       body: JSON.stringify(formData),
     })
-      .then((response) => response.json())
-      .then((newMovie) => console.log(newMovie));
+    .then((response) => {
+      if (response.ok) {
+        response.json().then((newMovie) => console.log(newMovie));
+      } else {
+        response.json().then((errorData) => setErrors(errorData.errors));
+      }
+    })
   }
 
   function handleChange(e) {
@@ -39,6 +45,13 @@ function MovieForm() {
   return (
     <Wrapper>
       <form onSubmit={handleSubmit}>
+        {errors.length > 0 && (
+        <ul style={{ color: "red" }}>
+          {errors.map((error) => (
+          <li key={error}>{error}</li>
+        ))}
+        </ul>
+        )}
         <FormGroup>
           <label htmlFor="title">Title</label>
           <input
